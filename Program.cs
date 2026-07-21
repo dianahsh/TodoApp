1d3c36faeec8f76b6e0c2d9c746298d1a326ee8b
dianahsh <heshmatidiyana@gmail.com>
2026-07-22 00:53:31 +0330

    Add Auth

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Todo.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 var connectionstr = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<TodoContext>(op => op.UseSqlServer(connectionstr));
+builder.Services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(connectionstr));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,6 +31,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
