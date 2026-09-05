a6e3a967896ae6db88774065fb5574ca91e59f14
dianahsh <heshmatidiyana@gmail.com>
2026-09-05 10:19:13 +0330

    from controller based to minimal api

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
 using Todo.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +42,52 @@ app.UseAuthorization();
 
 app.MapIdentityApi<IdentityUser>();
 
-app.MapControllers();
+var gr = app.MapGroup("/Todo");
+
+gr.MapGet("/all", async (TodoContext tdc) =>
+{
+    return await tdc.Todos.ToListAsync();
+});
+
+gr.MapGet("/{id}", async (TodoContext tdc, int id) =>
+{
+    return await tdc.Todos.FindAsync(id) is Todos todo ? Results.Ok(todo) : Results.NotFound();
+});
+
+gr.MapPost("", async (HttpContext context, TodoContext tdc, Todos t) =>
+{
+    var us = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+    t.UserId = us;
+    tdc.Todos.Add(t);
+    await tdc.SaveChangesAsync();
+    return Results.Ok();
+}).RequireAuthorization();
+
+gr.MapDelete("/{id}", async (int id, TodoContext tdc) =>
+{
+    var v = await tdc.Todos.FindAsync(id);
+    if(v is not null)
+    {
+        tdc.Todos.Remove(v);
+        await tdc.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+gr.MapPut("/{id}", async (TodoContext tdc, int id, Todos t) =>
+{
+    var v = await tdc.Todos.FindAsync(id);
+    if (v is not null)
+    {
+        v.Title = t.Title;
+        v.Description = t.Description;
+        v.UpdatedDate = DateTime.Now;
+        await tdc.SaveChangesAsync();
+        return Results.Ok();
+    }
+    return Results.NotFound();
+});
 
 app.Run();
